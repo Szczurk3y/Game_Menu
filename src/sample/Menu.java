6dcf767ea71e57dd485ba85c40ea049fcf7cccbc
maciek stoiński <maciek.stoinski2@gmail.com>
2018-11-06 22:13:25 +0100

    A little bit more advanced menu subtitles

--- src/sample/Menu.java
+++ src/sample/Menu.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -18,6 +19,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -28,9 +30,13 @@ public class Menu extends Application implements WindowInterface {
     private Stage stage;
     private Scene scene;
     private Pane root;
-    private WindowType windowType = WindowType.MENU;
+    private WindowType windowType;
     private ImageView imageView;
     private GameMenu gameMenu;
+
+    public Menu() {
+        windowType = WindowType.MENU;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -54,22 +60,68 @@ public class Menu extends Application implements WindowInterface {
         gameMenu.setVisible(true);
         root.getChildren().addAll(imageView, gameMenu);
 
+        stage.setFullScreen(windowType.isFullScreen());
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-
     }
 
     public class GameMenu extends Parent {
+        private final int offset = windowType.getWindowWidth()/2;
+
         public GameMenu() {
             VBox firstmenu = new VBox(10);
-            firstmenu.setTranslateX(menuWindowWidth/3 +18);
-            firstmenu.setTranslateY(menuWindowHeight/3 +15);
+            VBox secondmenu = new VBox(10);
 
-            MenuButton resumeButton = new MenuButton("RESUME");
+            firstmenu.setTranslateX(windowType.getWindowWidth()/2 - 125);
+            firstmenu.setTranslateY(windowType.getWindowHeight()/2 - 30);
+            secondmenu.setTranslateX(windowType.getWindowWidth());
+            secondmenu.setTranslateY(windowType.getWindowHeight()/2 - 30);
+
+            MenuButton resumeButton = new MenuButton("START");
             MenuButton optionButton = new MenuButton("OPTIONS");
             MenuButton exitButton = new MenuButton("EXIT");
+            MenuButton soundButton = new MenuButton("SOUND");
+            MenuButton backButton = new MenuButton("BACK");
+
+            optionButton.setOnMouseClicked(event -> {
+                getChildren().add(secondmenu);
+                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), firstmenu);
+                TranslateTransition tt2 = new TranslateTransition(Duration.seconds(0.35), secondmenu);
+
+                tt.setToX(firstmenu.getTranslateX() - offset);
+                tt2.setToX(windowType.getWindowWidth()/2 - 125);
+
+                tt.play();
+                tt2.play();
+
+                tt.setOnFinished(event1 -> {
+                    getChildren().remove(firstmenu);
+                });
+            });
+            exitButton.setOnMouseClicked(event -> {
+                System.exit(0);
+            });
+
+            backButton.setOnMouseClicked(event -> {
+                getChildren().add(firstmenu);
+                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), secondmenu);
+                TranslateTransition tt2 = new TranslateTransition(Duration.seconds(0.35), firstmenu);
+
+                tt.setToX(secondmenu.getTranslateX() + offset);
+                tt2.setToX(windowType.getWindowWidth()/2 - 125);
+
+                tt.play();
+                tt2.play();
+
+                tt.setOnFinished( event1 -> {
+                    getChildren().remove(secondmenu);
+                });
+
+            });
+
             firstmenu.getChildren().addAll(resumeButton, optionButton, exitButton);
+            secondmenu.getChildren().addAll(soundButton, backButton);
             getChildren().addAll(firstmenu);
         }
     }
--- src/sample/WindowType.java
+++ src/sample/WindowType.java
@@ -1,17 +1,22 @@
 package sample;
 
 public enum WindowType implements WindowInterface {
-    MENU(menuWindowWidth, menuWindowHeight, "Menu", "res/graphics/menu_background.jpg"), GAME(gameWindowWidth, gameWindowHeight, "Game", "res/graphics/menu_background.jpg");
+    MENU(menuWindowWidth, menuWindowHeight, "Menu", "res/graphics/menu_background.jpg", false),
+    GAME(gameWindowWidth, gameWindowHeight, "Game", "res/graphics/menu_background.jpg", true);
 
     private final int windowWidth, windowHeight;
     private final String windowTitle, windowImagePath;
+    private final boolean isFullScreen;
 
-    private WindowType(int width, int height, String title, String path) {
+    private WindowType(int width, int height, String title, String path, boolean isFullScreen) {
         this.windowTitle = title;
         this.windowWidth = width;
         this.windowHeight = height;
         this.windowImagePath = path;
+        this.isFullScreen = isFullScreen;
     }
+
+    public boolean isFullScreen() { return this.isFullScreen; }
 
     public int getWindowWidth() {
         return this.windowWidth;
